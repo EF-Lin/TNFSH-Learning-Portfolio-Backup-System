af4b2cd81cdebc691536b5d7817321d4197cf50f
EF <enfull2006@gmail.com>
2024-04-11 16:11:56 +0800

    將主程式寫於main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from surface import Interface
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('World')
+    main = Interface()
